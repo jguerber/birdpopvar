d86d528a27671dcdc91cfe1b168d9db64390ef20
Josquin Guerber <josquin.guerber@gmail.com>
2025-08-28 17:17:14 +0200

    better display when using build non-interactively

--- scripts/build.R
+++ scripts/build.R
@@ -6,7 +6,7 @@ print(paste(
 # renv::install("here", library = renv::paths$library())
 devtools::load_all()
 
-targets::tar_make()
+targets::tar_make(reporter = "terse") # use the non-interactive output mode
 
 # print worker info
 print(targets::tar_crew())
