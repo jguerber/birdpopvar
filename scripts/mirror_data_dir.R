443c288749f79f431e4f05c487e8103c93b74221
Josquin Guerber <josquin.guerber@gmail.com>
2025-09-03 14:18:40 +0200

    feat: helper script to copy data

---
+++ scripts/mirror_data_dir.R
@@ -0,0 +1,19 @@
+# Make the subdirectory ./data mirror the structure and contents of ../data (used to quickly import processed data in the code project)
+
+message(
+  "WARNING: this will remove the contents of ./data\n",
+  "and replace them with ../data.\n"
+)
+
+check_ok <- readline("Proceed ? type 'yes'")
+
+if (check_ok == "yes") {
+  unlink(here::here("data"), recursive = T)
+
+  dir.create(here::here("data"), recursive = T)
+  file.copy(here::here("../data"), here::here("data"), recursive = T)
+
+} else {
+  print("Cancelled by user")
+}
+
