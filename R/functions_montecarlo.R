22a60eb32609082b7e0e6594c989d3f9425b4dfd
Josquin Guerber <josquin.guerber@gmail.com>
2026-01-08 14:41:19 +0100

    feat: retry sampling if its model produces an error

--- R/functions_montecarlo.R
+++ R/functions_montecarlo.R
@@ -14,24 +14,43 @@ draw_mc_samples <- function(species_trends, covariates, reps) {
 }
 
 # single iteration : redraw all species trends in their estimated interval and fit model
-mc_iterate <- function(dat, covariates, .r = NA) {
-    dat <- dat %>% 
-        mutate(
-            trend_mc = rnorm(n = n(), mean = trend, sd = se)
-        ) %>% 
-        group_by(COMMUNITY_ID) %>% 
-        summarise(
-            abs_trend_average_mc = mean(abs(trend_mc))
-        ) %>% 
-        left_join(covariates, by = "COMMUNITY_ID") 
+mc_iterate <- function(dat, covariates, .r = NA, try_max = 3) {
   
-  model <- suppressWarnings({glmmTMB::glmmTMB(
-            formula = abs_trend_average_mc ~HABITAT_GROUP + N_POINTS_avg + (1|SITE2),
-            family = "lognormal",
-            data = dat
-        )})
+  errors <- 1
+  tries <- 0
+
+  while (errors > 0 && tries < try_max) {
+
+    dat_s <- dat %>% 
+      mutate(
+          trend_mc = rnorm(n = n(), mean = trend, sd = se)
+      ) %>% 
+      group_by(COMMUNITY_ID) %>% 
+      summarise(
+          abs_trend_average_mc = mean(abs(trend_mc))
+      ) %>% 
+      left_join(covariates, by = "COMMUNITY_ID") 
   
-  model %>% 
+    model <- catchConditions({glmmTMB::glmmTMB(
+      formula = abs_trend_average_mc ~HABITAT_GROUP + N_POINTS_avg + (1|SITE2),
+      family = "lognormal",
+      data = dat_s
+    )})
+
+    errors <- length(model$error)
+    tries <- tries + 1
+
+  }
+
+  # after while loop : either tries == try_max and model may be ok, or model succeeded
+
+  if (tries == try_max) {
+    if (errors > 0) {
+      stop(glue::glue("Model failed with {{ tries }} attempts"))
+    }
+  }
+
+  model$value %>% 
         broom.mixed::tidy() %>% 
         select(effect, term, group, estimate, std.error) %>% 
         mutate(
