f429556d84e29b2c41b0ee573fa1f3c1c4104d9f
Josquin Guerber <josquin.guerber@gmail.com>
2026-03-02 11:04:57 +0100

    feat: util for saving plots

--- R/functions_plot_utils.R
+++ R/functions_plot_utils.R
@@ -26,3 +26,20 @@ scale_habitats <- function(
 out2fig <- function(out.width, out.width.default = 0.6, fig.width.default = 6) {
   fig.width.default * out.width / out.width.default
 }
+
+#' Save a figure then return it
+#' 
+#' @param fig ggplot2 figure
+#' @param filename file path for ggsave
+#' @param ... arguments to ggplot2::ggsave. Match chunk parameters with units="in"
+#' if you want to reproduce a quarto output
+save_and_output <- function(fig, filename, ...) {
+
+  ggsave(
+    filename = filename,
+    plot = fig,
+    ...
+  )
+
+  fig
+}
